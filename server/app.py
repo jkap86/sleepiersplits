7080517d176721e74c8bc933398dff2ff7d0873e
Jay Kapoor <jkap86@gmail.com>
2023-10-05 02:43:51 -0400

    -

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app = Flask(__name__, static_folder='../client/build', static_url_path='/')
+app = Flask(__name__, static_folder='./client/build', static_url_path='/')
 
 @app.route('/')
 def index():
