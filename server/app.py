6b6e93bbe30edc97d6cd162de73c6d877e8175fd
Jay Kapoor <jkap86@gmail.com>
2023-10-11 12:43:08 -0400

    -

--- server/app.py
+++ server/app.py
@@ -2,11 +2,16 @@ from flask import Flask, request, jsonify
 import pandas as pd
 from flask_cors import CORS
 import os
+import threading
+import time
+import requests
+
 
 app = Flask(__name__)
 CORS(app)
 
 app = Flask(__name__, static_folder='../client/build', static_url_path='/')
+
 
 @app.route('/cleanfile/<int:season>', methods=['GET'])
 def cleanfile(season):
@@ -42,41 +47,43 @@ def cleanfile(season):
             "down",
             "desc"
         ]
-        
+
         columns2 = [
             "play_id",
             "game_id",
             'offense_personnel',
-              'offense_players',
-                'defenders_in_box',
-                  'defense_personnel',
-                    'defense_players'
-                    ]
+            'offense_players',
+            'defenders_in_box',
+            'defense_personnel',
+            'defense_players'
+        ]
 
-       
         input_file_path1 = f'./play_by_play_{season}.csv.gz'
         input_file_path2 = f'./pbp_participation_{season}.csv'
         output_file_path = f'./all_pbp.csv.gz'
 
-        df1 = pd.read_csv(input_file_path1, compression='gzip', low_memory=False)
+        df1 = pd.read_csv(input_file_path1,
+                          compression='gzip', low_memory=False)
         df1 = df1[columns1]
 
         df2 = pd.read_csv(input_file_path2)
         df2 = df2[columns2]
 
-        df = pd.merge(df1, df2, on=['play_id', 'game_id'], how='inner') 
+        df = pd.merge(df1, df2, on=['play_id', 'game_id'], how='inner')
 
         df_all = pd.read_csv(output_file_path, compression='gzip')
         df_all_updated = pd.concat([df_all, df], ignore_index=True)
 
         df_all_updated = df_all_updated.drop_duplicates()
 
-        df_all_updated.to_csv(output_file_path, compression='gzip', index=False)
+        df_all_updated.to_csv(
+            output_file_path, compression='gzip', index=False)
 
         return jsonify({"message": f"File for {season} processed successfully!"})
-    
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 @app.route('/player/recsummary', methods=['POST'])
 def receiving():
@@ -85,7 +92,7 @@ def receiving():
     startWeek = int(request.json.get('startWeek'))
     endSeason = int(request.json.get('endSeason'))
     endWeek = int(request.json.get('endWeek'))
-    
+
     file = './all_pbp.csv.gz'
 
     df = pd.read_csv(file, compression='gzip', low_memory=False)
@@ -103,7 +110,7 @@ def receiving():
                 df['season'].gt(startSeason)
             )
         ) & (
-             (
+            (
                 (df['season'].eq(endSeason)) &
                 (df['week'].le(endWeek))
             ) | (
@@ -111,38 +118,37 @@ def receiving():
             )
         )
     ]
-    
+
     for p in request.json.get('include'):
         if p != "0":
-            df_range = df_range[df_range['offense_players'].str.contains(p['gsis_id'], na=False)]
-
-            
+            df_range = df_range[df_range['offense_players'].str.contains(
+                p['gsis_id'], na=False)]
 
     for p in request.json.get('exclude'):
         if p != "0":
-            df_range = df_range[~df_range['offense_players'].str.contains(p['gsis_id'], na=False)]
+            df_range = df_range[~df_range['offense_players'].str.contains(
+                p['gsis_id'], na=False)]
 
     participation = df_range[
-       ( df_range['offense_players'].str.contains(f"{player_id}", na=False)) & 
-       (df_range['pass_attempt'] == 1) 
+        (df_range['offense_players'].str.contains(f"{player_id}", na=False)) &
+        (df_range['pass_attempt'] == 1)
     ]
 
-   
     games = participation['game_id'].nunique()
     routes = len(participation)
 
-    targeted =  df_range[
-        (df_range['receiver_player_id'] == player_id) 
-        ]
+    targeted = df_range[
+        (df_range['receiver_player_id'] == player_id)
+    ]
     targets = len(targeted)
 
-    rec = targeted[targeted['complete_pass'] == 1]  
+    rec = targeted[targeted['complete_pass'] == 1]
     rec_yds = rec['receiving_yards'].sum()
     rec_td = rec['pass_touchdown'].sum()
 
-    air_yards = df_range[df_range['receiver_player_id'] == player_id]['air_yards'].sum()
+    air_yards = df_range[df_range['receiver_player_id']
+                         == player_id]['air_yards'].sum()
 
-    
     def getKey():
         if request.json.get('breakoutby') == 'aDot':
             return 'air_yards_range'
@@ -155,29 +161,33 @@ def receiving():
 
     games_all = []
 
-    if any(e in request.json.get('breakoutby') for e in ['season', 'passer_player_id'] ):
+    if any(e in request.json.get('breakoutby') for e in ['season', 'passer_player_id']):
         games_b = {}
 
         for group_name, group_data in participation.groupby(key):
             games_b[group_name] = {}
             games_b[group_name][key] = group_name
             games_b[group_name]["routes"] = len(group_data)
-            games_b[group_name]["games"] =  group_data['game_id'].nunique()
-           
+            games_b[group_name]["games"] = group_data['game_id'].nunique()
 
         for group_name, group_data in targeted.groupby(key):
-          
+
             games_b[group_name]["targets"] = len(group_data)
-            games_b[group_name]["receptions"] = len(group_data[group_data['complete_pass'] == 1])
-            games_b[group_name]["receiving_yards"] = group_data[group_data['complete_pass'] == 1]['receiving_yards'].sum()
-            games_b[group_name]["touchdowns"] = group_data[group_data['complete_pass'] == 1]['pass_touchdown'].sum()
-            games_b[group_name]["air_yards"] = group_data[group_data['receiver_player_id'] == player_id]['air_yards'].sum()
+            games_b[group_name]["receptions"] = len(
+                group_data[group_data['complete_pass'] == 1])
+            games_b[group_name]["receiving_yards"] = group_data[group_data['complete_pass']
+                                                                == 1]['receiving_yards'].sum()
+            games_b[group_name]["touchdowns"] = group_data[group_data['complete_pass']
+                                                           == 1]['pass_touchdown'].sum()
+            games_b[group_name]["air_yards"] = group_data[group_data['receiver_player_id']
+                                                          == player_id]['air_yards'].sum()
 
         games_all = list(games_b.values())
     elif request.json.get('breakoutby') == 'formation':
-    
+
         for group in ['1 WR', '2 WR', '3 WR', '4 WR']:
-            targeted_b = targeted[targeted['offense_personnel'].str.contains(group)]
+            targeted_b = targeted[targeted['offense_personnel'].str.contains(
+                group)]
             rec_b = targeted_b[targeted_b['complete_pass'] == 1]
 
             games_all.append({
@@ -189,14 +199,14 @@ def receiving():
                 "receiving_yards": rec_b['receiving_yards'].sum(),
                 "touchdowns": rec_b['pass_touchdown'].sum(),
                 "air_yards": df_range[
-                    (df_range['receiver_player_id'] == player_id) & 
+                    (df_range['receiver_player_id'] == player_id) &
                     (df_range['offense_personnel'].str.contains(group))
-                    ]['air_yards'].sum()
+                ]['air_yards'].sum()
             })
     elif request.json.get('breakoutby') == 'aDot':
         targeted_under_5 = targeted[targeted['air_yards'] < 5]
         rec_under_5 = targeted_under_5[targeted_under_5['complete_pass'] == 1]
-        
+
         games_all.append({
             "air_yards_range": "Under 5 Yd",
             "games": participation[participation['air_yards'] < 5]['game_id'].nunique(),
@@ -206,12 +216,13 @@ def receiving():
             "receiving_yards": rec_under_5['receiving_yards'].sum(),
             "touchdowns": rec_under_5['pass_touchdown'].sum(),
             "air_yards": df_range[
-                (df_range['receiver_player_id'] == player_id) & 
+                (df_range['receiver_player_id'] == player_id) &
                 (df_range['air_yards'] < 5)
-                ]['air_yards'].sum()
+            ]['air_yards'].sum()
         })
 
-        targeted_5_10 = targeted[(targeted['air_yards'] >= 5) & (participation['air_yards'] <= 10)]
+        targeted_5_10 = targeted[(targeted['air_yards'] >= 5) & (
+            participation['air_yards'] <= 10)]
         rec_5_10 = targeted_5_10[targeted_5_10['complete_pass'] == 1]
 
         games_all.append({
@@ -223,13 +234,14 @@ def receiving():
             "receiving_yards": rec_5_10['receiving_yards'].sum(),
             "touchdowns": rec_5_10['pass_touchdown'].sum(),
             "air_yards": df_range[
-                (df_range['receiver_player_id'] == player_id) & 
-                (df_range['air_yards'] >= 5) & 
+                (df_range['receiver_player_id'] == player_id) &
+                (df_range['air_yards'] >= 5) &
                 (participation['air_yards'] <= 10)
-                ]['air_yards'].sum()
+            ]['air_yards'].sum()
         })
 
-        targeted_10_15 = targeted[(targeted['air_yards'] >= 10) & (participation['air_yards'] <= 15)]
+        targeted_10_15 = targeted[(targeted['air_yards'] >= 10) & (
+            participation['air_yards'] <= 15)]
         rec_10_15 = targeted_10_15[targeted_10_15['complete_pass'] == 1]
 
         games_all.append({
@@ -241,15 +253,15 @@ def receiving():
             "receiving_yards": rec_10_15['receiving_yards'].sum(),
             "touchdowns": rec_10_15['pass_touchdown'].sum(),
             "air_yards": df_range[
-                (df_range['receiver_player_id'] == player_id) & 
-                (df_range['air_yards'] >= 10) & 
+                (df_range['receiver_player_id'] == player_id) &
+                (df_range['air_yards'] >= 10) &
                 (participation['air_yards'] <= 15)
-                ]['air_yards'].sum()
+            ]['air_yards'].sum()
         })
 
         targeted_over_15 = targeted[targeted['air_yards'] > 15]
         rec_over_15 = targeted_over_15[targeted_over_15['complete_pass'] == 1]
-        
+
         games_all.append({
             "air_yards_range": "Under 5 Yd",
             "games": participation[participation['air_yards'] > 15]['game_id'].nunique(),
@@ -259,21 +271,21 @@ def receiving():
             "receiving_yards": rec_over_15['receiving_yards'].sum(),
             "touchdowns": rec_over_15['pass_touchdown'].sum(),
             "air_yards": df_range[
-                (df_range['receiver_player_id'] == player_id) & 
+                (df_range['receiver_player_id'] == player_id) &
                 (df_range['air_yards'] > 15)
-                ]['air_yards'].sum()
+            ]['air_yards'].sum()
         })
 
-        targeted_5_10 = targeted[(targeted['air_yards'] >= 5) & (participation['air_yards'] <= 10)]
+        targeted_5_10 = targeted[(targeted['air_yards'] >= 5) & (
+            participation['air_yards'] <= 10)]
         rec_5_10 = targeted_5_10[targeted_5_10['complete_pass'] == 1]
 
-
     return jsonify(
-    
+
         {
-            **request.json,        
-            "routes": routes, 
-            "games": games, 
+            **request.json,
+            "routes": routes,
+            "games": games,
             "targets": targets,
             "receptions": len(rec),
             "receiving_yards": rec_yds,
@@ -282,6 +294,7 @@ def receiving():
             "player_breakoutby": games_all
         }
     )
+
 
 @app.route('/receiving/top50', methods=['POST'])
 def top50_receiving():
@@ -308,7 +321,7 @@ def top50_receiving():
                 df['season'].gt(startSeason)
             )
         ) & (
-             (
+            (
                 (df['season'].eq(endSeason)) &
                 (df['week'].le(endWeek))
             ) | (
@@ -316,13 +329,14 @@ def top50_receiving():
             )
         )
     ]
-    
+
     result = {
-        **request.json,   
-        "top50": [] 
+        **request.json,
+        "top50": []
     }
 
-    top50 = df_range.groupby('receiver_player_id')['receiving_yards'].sum().nlargest(50)
+    top50 = df_range.groupby('receiver_player_id')[
+        'receiving_yards'].sum().nlargest(50)
     top50 = top50.index.tolist()
 
     for receiver in top50:
@@ -341,6 +355,7 @@ def top50_receiving():
 
     return jsonify(result)
 
+
 @app.route('/player/rushsummary', methods=['POST'])
 def rushing():
     player_id = request.json.get('player_id')
@@ -348,7 +363,7 @@ def rushing():
     startWeek = int(request.json.get('startWeek'))
     endSeason = int(request.json.get('endSeason'))
     endWeek = int(request.json.get('endWeek'))
-    
+
     file = './all_pbp.csv.gz'
 
     df = pd.read_csv(file, compression='gzip', low_memory=False)
@@ -366,7 +381,7 @@ def rushing():
                 df['season'].gt(startSeason)
             )
         ) & (
-             (
+            (
                 (df['season'].eq(endSeason)) &
                 (df['week'].le(endWeek))
             ) | (
@@ -374,17 +389,16 @@ def rushing():
             )
         )
     ]
-    
+
     for p in request.json.get('include'):
         if p != "0":
-            df_range = df_range[df_range['offense_players'].str.contains(p['gsis_id'], na=False)]
-
-            
+            df_range = df_range[df_range['offense_players'].str.contains(
+                p['gsis_id'], na=False)]
 
     for p in request.json.get('exclude'):
         if p != "0":
-            df_range = df_range[~df_range['offense_players'].str.contains(p['gsis_id'], na=False)]
-
+            df_range = df_range[~df_range['offense_players'].str.contains(
+                p['gsis_id'], na=False)]
 
     return jsonify({
         **request.json,
@@ -398,14 +412,31 @@ def rushing():
         "games": df_range[df_range['receiver_player_id'] == player_id]['game_id'].nunique()
     })
 
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
 
 @app.route('/<path>')
 def catch_all(path):
     return app.send_static_file('index.html')
 
+
+def ping_server():
+    while True:
+        time.sleep(2 * 60)  # Sleep for 29 minutes
+        try:
+            response = requests.get(
+                'https://sleepiersplits-73b0aab383b9.herokuapp.com/')
+            print(f"Pinged server with status code: {response.status_code}")
+        except requests.RequestException as e:
+            print(f"Failed to ping server: {e}")
+
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000)) 
+    t = threading.Thread(target=ping_server)
+    t.start()
+
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
